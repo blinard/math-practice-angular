0960e14844f02ede2dece5791e14846ab942533f
Angular CLI <angular-cli@angular.io>
2017-05-28 12:19:59 -0700

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MathPracticeAngularPage } from './app.po';
+
+describe('math-practice-angular App', () => {
+  let page: MathPracticeAngularPage;
+
+  beforeEach(() => {
+    page = new MathPracticeAngularPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
